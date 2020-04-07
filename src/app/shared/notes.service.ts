d420ef2ac53d1bdb0c91d34b949f8d17c3ea2903
Mon0thing <mohamed.nouri.1997@gmail.com>
2020-04-08 00:15:04 +0100

    added local storage

--- src/app/shared/notes.service.ts
+++ src/app/shared/notes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, HostListener } from "@angular/core";
 import { Note } from "./note.model";
 
 @Injectable({
@@ -8,9 +8,20 @@ export class NotesService {
   notes: Note[] = new Array<Note>();
 
   constructor() {
-    for (let i = 0; i < 10; i++) {
-      this.notes.push(new Note("note " + i, "body for note " + i));
+    // for (let i = 0; i < 10; i++) {
+    //   this.notes.push(new Note("note " + i, "body for note " + i));
+    // }
+    this.notes = JSON.parse(localStorage.getItem("notes"));
+    if (!this.notes) {
+      this.notes = [];
     }
+  }
+
+  /**
+   * Update Local Storage
+   */
+  updateStorage() {
+    localStorage.setItem("notes", JSON.stringify(this.notes));
   }
 
   getAll(): Note[] {
@@ -24,15 +35,16 @@ export class NotesService {
   // }
   add(note: Note) {
     let newLength = this.notes.push(note);
-    return newLength;
+    this.updateStorage();
   }
   update(id: number, title: string, body: string) {
     let note = this.get(id);
     note.title = title;
     note.body = body;
+    this.updateStorage();
   }
   delete(id: number) {
     this.notes = this.notes.filter((n) => n.id != id);
-    console.log(this.notes.length);
+    this.updateStorage();
   }
 }
